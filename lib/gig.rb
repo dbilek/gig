6252e8092038550a87346861826f3176c8c27606
Danjo Bilek <danjo@axiomq.com>
2019-06-10 11:27:10 +0200

    Added Greper class along with gig executable file

--- lib/gig.rb
+++ lib/gig.rb
@@ -1,6 +1,11 @@
 require "gig/version"
+require "gig/greper"
+require 'pry'
 
 module Gig
-  class Error < StandardError; end
-  # Your code goes here...
+  # class Error < StandardError; end
+  def self.call(options = [])
+    greper = Greper.new(options)
+    greper.grep
+  end
 end
---
+++ lib/gig/greper.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+module Gig
+  class Greper
+    attr_reader :options
+
+    def initialize(options = [])
+    end
+
+    def grep
+      puts "Grep message"
+    end
+  end
+end
